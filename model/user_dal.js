1fa29f6f07d955404c3ac779e0955fd67d9319e9
Verified ✔️ <AlexBeJoyful>
2017-12-14 00:21:03 -0800

    got users to view

--- model/user_dal.js
+++ model/user_dal.js
@@ -7,8 +7,16 @@ var connection = mysql.createConnection(db.config);
 exports.getAll = function(callback) {
     var query = 'SELECT * FROM user;';
 
-    connection.query(query, function(err, result) {
-        callback(err, result);
+    connection.query(query, function(err, res) {
+        callback(err, res);
     });
 };
 
+
+exports.getAllById = function(id, callback) {
+    var query = "select first_name, last_name, email from user where user_id = " + id + ";";
+    console.log("query: " + query);
+    connection.query(query, function(err, res){
+        callback(err, res);
+    })
+};
--- routes/user.js
+++ routes/user.js
@@ -9,17 +9,38 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/all', function(req, res) {
-  user_dal.getAll(function(err, result) {
-    if(err) {
+  user_dal.getAll(function(err1, res1) {
+    if(err1) {
 
-      res.send(err);
+      res.send(err1);
     }
     else {
 
-      res.render('user/userViewAll', {'result': result});
+      res.render('user/userViewAll', {response: res1});
     }
   });
 });
+
+router.get('/viewBy/:id', function(req, res){
+  var id = req.params.id;
+
+  user_dal.getAllById(id, function(err2, user) {
+      if (err2) {
+          console.log("error");
+          res.send(err2);
+      }
+      console.log(user);
+      var obj = {
+          user: user,
+          user_id: id};
+
+      console.log(obj);
+
+      res.render('user/userViewById', {object: obj});
+
+    })
+})
+
 
 
 module.exports = router;
